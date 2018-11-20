d5338d80888eb2418d9349771d1c7fed98859fcf
Carl <carlospidla@gmail.com>
2018-11-19 19:09:57 -0500

    added Carl_Scene_Nov_19 with ui health for all characters. Damage is handled in healthsubtractor.cs and healthmanager.cs

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour {
+
+	public ProgressBar healthBar;
+	public ProgressBarCircle healthCircle;
+	public int health = 100;
+
+	void Start()
+    {
+		if (healthBar != null)
+		{
+			healthBar.BarValue = health;
+		}
+
+		if (healthCircle != null)
+        {
+            healthCircle.BarValue = health;
+        }
+    }
+
+	public void LoseHealth (int damage) {
+		if (healthBar != null)
+		{
+			health += -damage;
+			healthBar.BarValue = health;
+		}
+
+		if (healthCircle != null)
+        {
+            health += -damage;
+			healthCircle.BarValue = health;
+        }
+	}
+}
---
+++ Assets/Scripts/HealthSubtractor.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSubtractor : MonoBehaviour
+{
+
+	public int punchDamage = 15;
+	bool canDamage = true;
+	HealthManager thisManager;
+	public float delayTime = 1.0f;
+	float startDelayTime = 1.0f;
+
+
+    void Start()
+    {
+		startDelayTime = delayTime;
+		thisManager = GetComponentInParent<HealthManager>();
+    }
+
+	 void Update()
+	{
+		if (canDamage == false) DamageWait();
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if (collision.gameObject.layer == 9)
+		{
+			if (canDamage == true)
+			{
+				print("damage");
+				//Ensure we don't change our own script
+				if (collision.gameObject.transform.GetComponentInParent<HealthManager>() != thisManager)
+				{
+					collision.gameObject.transform.GetComponentInParent<HealthManager>().LoseHealth(punchDamage);
+					canDamage = false;   //wait a second so we dont trigger multiple instances
+
+				}
+			}
+		}
+	}
+
+	void DamageWait(){
+		delayTime -= Time.deltaTime;
+		if (delayTime <= 0){
+			canDamage = true;
+			delayTime = startDelayTime;
+		}
+	}
+}
