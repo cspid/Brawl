df0c80519bfdab64ca59be85203c1e272b7f3419
GBC Pier <poboivin2@gmail.com>
2018-11-22 13:25:48 -0500

    added content

--- Assets/Scripts/swapDestructableWall.cs
+++ Assets/Scripts/swapDestructableWall.cs
@@ -13,13 +13,18 @@ public class swapDestructableWall : MonoBehaviour
 		if (isBroken == false)
 			if (other.gameObject.layer == 9)
 			{
-                this.GetComponent<Collider>().isTrigger = true;
 				for (int i = 0; i < transform.childCount; i++)
 				{
 					transform.GetChild(i).GetComponent<Rigidbody>().isKinematic = false;
 					if (i == transform.childCount - 1) isBroken = true;
 
 				}
+                Invoke("turnKinematic", 0.1f);
 			}
 	}
+    void turnKinematic()
+    {
+        this.GetComponent<Collider>().isTrigger = true;
+
+    }
 }
